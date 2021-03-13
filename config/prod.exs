b965c9201240737823a76f81ec5561dd24b18d54
Tayvon Lewis <tayvonlewis0@gmail.com>
2021-03-12 20:15:23 -0600

    set host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :todo_list, TodoListWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "https://todo-list.gigalixirapp.com/", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
